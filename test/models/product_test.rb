98d13eb8c24539e69364b7c671802c32be0e8fce
fernanda <fernandakagami@gmail.com>
2018-08-07 17:20:31 -0300

    tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  	
+  	def setup
+    	@product = Product.new(name: "salada")
+  	end
+
+  	test "should be valid" do
+    	assert @product.valid?
+  	end
+
+  	test "name should be present" do
+    	@product.name = ""
+    	assert_not @product.valid?
+  	end
 end
